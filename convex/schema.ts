de10fbc737b1739bc1511548760e94295a45445c
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-11-01 01:38:27 -0400

    new commit

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,19 +4,19 @@ import { v } from "convex/values";
 
 const schema = defineSchema({
   ...authTables,
-  users: defineTable({
-    name: v.optional(v.string()),
-    image: v.optional(v.string()),
-    email: v.optional(v.string()),
-    emailVerificationTime: v.optional(v.number()),
-    phone: v.optional(v.string()),
-    phoneVerificationTime: v.optional(v.number()),
-    isAnonymous: v.optional(v.boolean()),
-    numOwned: v.optional(v.number()),
-    numLiked: v.optional(v.number()),
-    ownedSets: v.optional(v.array(v.id("sets"))),
-    likedSets: v.optional(v.array(v.id("sets"))),
-  }),
+  // users: defineTable({
+  //   name: v.optional(v.string()),
+  //   image: v.optional(v.string()),
+  //   email: v.optional(v.string()),
+  //   emailVerificationTime: v.optional(v.number()),
+  //   phone: v.optional(v.string()),
+  //   phoneVerificationTime: v.optional(v.number()),
+  //   isAnonymous: v.optional(v.boolean()),
+  //   numOwned: v.optional(v.number()),
+  //   numLiked: v.optional(v.number()),
+  //   ownedSets: v.optional(v.array(v.id("sets"))),
+  //   likedSets: v.optional(v.array(v.id("sets"))),
+  // }),
   sets: defineTable({
     name: v.string(),
     description: v.string(),
